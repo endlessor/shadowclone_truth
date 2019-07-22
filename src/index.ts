b349360ac8f4c4f8ff4af3199b9e1994f847e111
reopard226 <robcoder@hotmail.com>
2019-07-23 03:35:37 +0400

    Create build configuration for live deploy

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
 import { prisma } from '../generated/prisma-client'
 import schema from './schema'
+import express = require('express')
+import path = require('path')
+import bodyParser = require('body-parser')
+
 require('dotenv').config()
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/', express.static(path.join(path.dirname(__dirname), '/client/build')));
 
 const server = new ApolloServer({
   schema,
   context: { prisma },
 })
 
+server.applyMiddleware({ app });
+
 const port = process.env.SERVER_PORT || 4000
 
-server.listen({ port: port }, () =>
-  console.log(`🚀 Server ready at http://localhost:${port}`),
-)
+app.listen({ port: port }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
