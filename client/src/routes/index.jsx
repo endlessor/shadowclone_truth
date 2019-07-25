691aca7d68a8d5c0e9311cf6a0f52d11bee40669
codemaster6152 <punctualityman@outlook.com>
2019-07-25 21:20:24 +0100

    add result page

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import PreVote from "../pages/Prevote";
 import VoteReasoning from "../pages/VoteReasoning";
+import Results from "../pages/Results";
 
 const MainRoute = () => (
   <Router>
     <Route path="/" exact component={PreVote} />
     <Route path="/candidate/:id" component={VoteReasoning} />
+    <Route path="/result" component={Results} />
   </Router>
 );
 
