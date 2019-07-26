9ac64a053621a4827b0fd6eb7e400f11914e1a99
codemaster6152 <punctualityman@outlook.com>
2019-07-26 20:29:48 +0100

    add login page

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "../pages/Login";
 import PreVote from "../pages/Prevote";
 import VoteReasoning from "../pages/VoteReasoning";
 
 const MainRoute = () => (
   <Router>
     <Route path="/" exact component={PreVote} />
+    <Route path="/login" exact component={Login} />
     <Route path="/candidate/:id" component={VoteReasoning} />
   </Router>
 );
