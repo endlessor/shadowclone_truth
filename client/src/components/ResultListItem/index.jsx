dc10278efafb5e1516aa06d782a2cec2025f241d
codemaster6152 <punctualityman@outlook.com>
2019-07-27 09:41:19 +0100

    update components

--- client/src/components/ResultListItem/index.jsx
+++ client/src/components/ResultListItem/index.jsx
@@ -8,19 +8,19 @@ function ResultListItem({ data }) {
     <Card>
       <div className="p-grid p-align-center">
         <div className="p-col-4">
-          <Avatar url={data.photo} alt="avatar" />
+          <Avatar url={data.photo} alt="avatar" label={data.vote_type} />
         </div>
         <div className="p-col-8">
           <h4>{data.name}</h4>
           <div className="p-grid">
             <div className="p-col-4">
-              <TextItem label="POLLS" value={data.latest_poll} />
+              <TextItem label="POLLS" value={`${data.latest_poll}%`} />
             </div>
             <div className="p-col-4">
-              <TextItem label="VS.TRUMP" value={data.latest_odds} />
+              <TextItem label="VS.TRUMP" value={`${data.latest_odds}%`} />
             </div>
             <div className="p-col-4">
-              <TextItem label="PREVOTES" value={data.prevote_score} />
+              <TextItem label="PREVOTES" value={`${data.prevote_score}%`} />
             </div>
           </div>
         </div>
