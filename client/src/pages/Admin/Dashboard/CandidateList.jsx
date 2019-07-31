b87e25e04ca4d7569691e731399b09e701cf78d8
codemaster6152 <punctualityman@outlook.com>
2019-07-31 21:35:55 +0100

    add admin dashboard

---
+++ client/src/pages/Admin/Dashboard/CandidateList.jsx
@@ -0,0 +1,231 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { graphql, compose } from "react-apollo";
+import { DataTable } from "primereact/datatable";
+import { Button } from "primereact/button";
+import { Column } from "primereact/column";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+
+import {
+  AdminCandidatesQuery,
+  AdminAddCandidate,
+  AdminDeleteCandidate,
+  AdminUpdateCandidate
+} from "../../../queries";
+
+function CandidateList(props) {
+  const [newCandidate, setNewCandidate] = useState(false);
+  const [displayDialog, setDisplayDialog] = useState(false);
+  const [candidate, setCandidate] = useState(null);
+
+  const saveCandidate = () => {
+    const { addCandidate, updateCandidate } = props;
+    const { id, __typename, ...rest } = candidate;
+    if (newCandidate) {
+      addCandidate({
+        variables: {
+          data: { ...rest, age: parseInt(rest.age) }
+        }
+      }).then(() => setDisplayDialog(false));
+    } else {
+      updateCandidate({
+        variables: {
+          data: { ...rest, age: parseInt(rest.age) },
+          where: {
+            id
+          }
+        }
+      }).then(() => setDisplayDialog(false));
+    }
+  };
+
+  const deleteCandidate = () => {
+    const { deleteCandidate } = props;
+    deleteCandidate({
+      variables: {
+        where: {
+          id: candidate.id
+        }
+      }
+    }).then(() => setDisplayDialog(false));
+  };
+
+  const updateProperty = (property, value) => {
+    setCandidate({
+      ...candidate,
+      [property]: value
+    });
+  };
+
+  const addNewCandidate = () => {
+    setNewCandidate(true);
+    setCandidate({
+      name: "",
+      party: "",
+      state: "",
+      current_office: "",
+      age: 0
+    });
+    setDisplayDialog(true);
+  };
+
+  const onCandidateSelect = e => {
+    setNewCandidate(false);
+    setCandidate(Object.assign({}, e.data.candidate));
+    setDisplayDialog(true);
+  };
+
+  const {
+    candidates: { loading, candidatesWithVotes }
+  } = props;
+  return (
+    <React.Fragment>
+      <DataTable
+        lazy
+        rows={10}
+        paginator
+        loading={loading}
+        value={candidatesWithVotes}
+        header={<h2 style={{ margin: 0 }}>Candidates</h2>}
+        footer={
+          <div className="p-clearfix">
+            <Button
+              style={{ float: "left" }}
+              label="Add"
+              icon="pi pi-plus"
+              onClick={addNewCandidate}
+            />
+          </div>
+        }
+        selectionMode="single"
+        onRowSelect={onCandidateSelect}
+      >
+        <Column field="candidate.name" header="Name" sortable />
+        <Column field="tops" header="Top Picks" sortable />
+        <Column field="favorites" header="Favorites" sortable />
+        <Column field="compromises" header="Compromises" sortable />
+        <Column field="vetos" header="Vetos" sortable />
+      </DataTable>
+
+      <Dialog
+        visible={displayDialog}
+        width="300px"
+        header="Candidate Details"
+        modal={true}
+        footer={
+          <div className="ui-dialog-buttonpane p-clearfix">
+            <Button
+              label="Delete"
+              icon="pi pi-times"
+              onClick={deleteCandidate}
+            />
+            <Button label="Save" icon="pi pi-check" onClick={saveCandidate} />
+          </div>
+        }
+        onHide={() => setDisplayDialog(false)}
+      >
+        {candidate && (
+          <div className="p-grid p-fluid">
+            <div className="p-col-4" style={{ padding: ".75em" }}>
+              <label htmlFor="name">Name</label>
+            </div>
+            <div className="p-col-8" style={{ padding: ".5em" }}>
+              <InputText
+                id="name"
+                onChange={e => {
+                  updateProperty("name", e.target.value);
+                }}
+                value={candidate.name}
+              />
+            </div>
+
+            <div className="p-col-4" style={{ padding: ".75em" }}>
+              <label htmlFor="party">Party</label>
+            </div>
+            <div className="p-col-8" style={{ padding: ".5em" }}>
+              <InputText
+                id="party"
+                onChange={e => {
+                  updateProperty("party", e.target.value);
+                }}
+                value={candidate.party}
+              />
+            </div>
+
+            <div className="p-col-4" style={{ padding: ".75em" }}>
+              <label htmlFor="state">State</label>
+            </div>
+            <div className="p-col-8" style={{ padding: ".5em" }}>
+              <InputText
+                id="state"
+                onChange={e => {
+                  updateProperty("state", e.target.value);
+                }}
+                value={candidate.state}
+              />
+            </div>
+
+            <div className="p-col-4" style={{ padding: ".75em" }}>
+              <label htmlFor="current_office">Current Office</label>
+            </div>
+            <div className="p-col-8" style={{ padding: ".5em" }}>
+              <InputText
+                id="current_office"
+                onChange={e => {
+                  updateProperty("current_office", e.target.value);
+                }}
+                value={candidate.current_office}
+              />
+            </div>
+
+            <div className="p-col-4" style={{ padding: ".75em" }}>
+              <label htmlFor="age">Age</label>
+            </div>
+            <div className="p-col-8" style={{ padding: ".5em" }}>
+              <InputText
+                id="age"
+                onChange={e => {
+                  updateProperty("age", e.target.value);
+                }}
+                value={candidate.age}
+              />
+            </div>
+          </div>
+        )}
+      </Dialog>
+    </React.Fragment>
+  );
+}
+
+CandidateList.propTypes = {};
+
+export default compose(
+  graphql(AdminCandidatesQuery, { name: "candidates" }),
+  graphql(AdminAddCandidate, {
+    name: "addCandidate",
+    options: { refetchQueries: [{ query: AdminCandidatesQuery }] }
+  }),
+  graphql(AdminUpdateCandidate, {
+    name: "updateCandidate",
+    options: { refetchQueries: [{ query: AdminCandidatesQuery }] }
+  }),
+  graphql(AdminDeleteCandidate, {
+    name: "deleteCandidate",
+    options: {
+      update: (proxy, { data: { deleteCandidate } }) => {
+        const { candidatesWithVotes } = proxy.readQuery({
+          query: AdminCandidatesQuery
+        });
+        proxy.writeQuery({
+          query: AdminCandidatesQuery,
+          data: {
+            candidatesWithVotes: candidatesWithVotes.filter(
+              ({ candidate }) => candidate.id !== deleteCandidate.id
+            )
+          }
+        });
+      }
+    }
+  })
+)(CandidateList);
---
+++ client/src/pages/Admin/Dashboard/index.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Query } from "react-apollo";
+import { ProgressSpinner } from "primereact/progressspinner";
+import { Card } from "primereact/card";
+
+import { AdminTotalUsers, AdminTotalAttributes } from "../../../queries";
+import CandidateList from "./CandidateList";
+import PositionList from "./PositionList";
+
+const Dashboard = props => {
+  return (
+    <div className="p-grid">
+      <section className="p-col-12">
+        <Query query={AdminTotalUsers}>
+          {({ loading: loadingUsers, data: { users } }) => (
+            <Query query={AdminTotalAttributes}>
+              {({ loading: loadingAttributes, data: { voteAttributes } }) => {
+                if (loadingUsers || loadingAttributes)
+                  return <ProgressSpinner />;
+                return (
+                  <div className="p-grid">
+                    <div className="p-md-3 p-col-6">
+                      <Card
+                        title="Prevotes"
+                        subTitle={voteAttributes.prevotes.toString()}
+                      />
+                    </div>
+                    <div className="p-md-3 p-col-6">
+                      <Card title="Users" subTitle={users.length.toString()} />
+                    </div>
+                    <div className="p-md-3 p-col-6">
+                      <Card
+                        title="Top Picks"
+                        subTitle={voteAttributes.topCount.toString()}
+                      />
+                    </div>
+                    <div className="p-md-3 p-col-6">
+                      <Card
+                        title="Favorites"
+                        subTitle={voteAttributes.favoriteCount.toString()}
+                      />
+                    </div>
+                    <div className="p-md-2 p-col-6">
+                      <Card
+                        title="Compromises"
+                        subTitle={voteAttributes.compromiseCount.toString()}
+                      />
+                    </div>
+                    <div className="p-md-2 p-col-6">
+                      <Card
+                        title="Vetos"
+                        subTitle={voteAttributes.vetoCount.toString()}
+                      />
+                    </div>
+                    <div className="p-md-2 p-col-6">
+                      <Card
+                        title="Average Top Picks"
+                        subTitle={voteAttributes.average_top.toString()}
+                      />
+                    </div>
+                    <div className="p-md-2 p-col-6">
+                      <Card
+                        title="Average Favorites"
+                        subTitle={voteAttributes.average_favorite.toString()}
+                      />
+                    </div>
+                    <div className="p-md-2 p-col-6">
+                      <Card
+                        title="Average Compromises"
+                        subTitle={voteAttributes.average_compromise.toString()}
+                      />
+                    </div>
+                    <div className="p-md-2 p-col-6">
+                      <Card
+                        title="Average Vetos"
+                        subTitle={voteAttributes.average_veto.toString()}
+                      />
+                    </div>
+                  </div>
+                );
+              }}
+            </Query>
+          )}
+        </Query>
+      </section>
+      <section className="p-col-12">
+        <CandidateList />
+      </section>
+      <section className="p-col-12">
+        <PositionList />
+      </section>
+    </div>
+  );
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
