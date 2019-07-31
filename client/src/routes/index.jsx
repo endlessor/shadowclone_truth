937c7ebd6dcd615383ccf1a9e4b6a01b29ff13dd
codemaster6152 <punctualityman@outlook.com>
2019-07-31 22:55:17 +0100

    update routes for admin

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -15,12 +15,17 @@ import { AUTH_TOKEN, IS_ADMIN } from "../config";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const isAuthenticated = !!localStorage.getItem(AUTH_TOKEN);
+  const isAdmin = localStorage.getItem(IS_ADMIN);
   return (
     <Route
       {...rest}
       render={props =>
         isAuthenticated ? (
-          <Component {...props} {...rest} />
+          props.location.pathname !== "/admin" && isAdmin ? (
+            <Redirect to="/admin" />
+          ) : (
+            <Component {...props} />
+          )
         ) : (
           <Redirect
             to={{ pathname: "/login", state: { from: props.location } }}
@@ -31,23 +36,20 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
   );
 };
 
-const MainRoute = () => (
-  <Router>
-    <Switch>
-      <PrivateRoute
-        path="/"
-        exact
-        component={Redirect}
-        to={localStorage.getItem(IS_ADMIN) ? "/admin" : "/prevote"}
-      />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-      <PrivateRoute path="/prevote" component={PreVote} />
-      <PrivateRoute path="/candidate/:id" component={VoteReasoning} />
-      <PrivateRoute path="/result" component={Results} />
-      <PrivateRoute path="/admin" component={AdminRoutes} />
-    </Switch>
-  </Router>
-);
+const MainRoute = () => {
+  return (
+    <Router>
+      <Switch>
+        <PrivateRoute path="/" exact />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <PrivateRoute path="/prevote" component={PreVote} />
+        <PrivateRoute path="/candidate/:id" component={VoteReasoning} />
+        <PrivateRoute path="/result" component={Results} />
+        <PrivateRoute path="/admin" component={AdminRoutes} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default MainRoute;
