c2847d250d962197321726f7f459f30ad4039d0c
codemaster6152 <punctualityman@outlook.com>
2019-08-06 01:42:27 +0100

    update prevote page

--- client/src/pages/Prevote/index.jsx
+++ client/src/pages/Prevote/index.jsx
@@ -5,10 +5,9 @@ import { DataView } from "primereact/dataview";
 import { Button } from "primereact/button";
 
 import { CandidateListItem } from "../../components";
-import {
-  CandidateQuery,
-  UserVoteMutation
-} from "../../queries/candidate";
+import { CandidateQuery, UserVoteMutation } from "../../queries/candidate";
+
+import "./Prevote.style.scss";
 
 function PreVote({ history }) {
   const itemTemplate = (candidate, layout) => {
@@ -44,30 +43,30 @@ function PreVote({ history }) {
   };
 
   return (
-      <div className="p-col-12 p-sm-12 p-md-6 page">
-        <div className="p-grid p-justify-between p-align-center">
-          <div className="p-col-8">
-            <h1>Prevoting</h1>
-          </div>
-          <div className="p-col-4 p-fluid">
-            <Button label="Next" onClick={handleNext} />
-          </div>
+    <div className="p-col-12 p-sm-12 p-md-6 page prevote">
+      <div className="p-grid p-justify-between p-align-center">
+        <div className="p-col-8">
+          <h1>Prevoting</h1>
         </div>
-        <Query query={CandidateQuery} fetchPolicy="network-only">
-          {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error : {error}</p>;
-            return (
-              <DataView
-                value={data.candidates}
-                layout="list"
-                itemTemplate={itemTemplate}
-                rows={20}
-              />
-            );
-          }}
-        </Query>
+        <div className="p-col-4 p-fluid">
+          <Button label="Next" onClick={handleNext} />
+        </div>
       </div>
+      <Query query={CandidateQuery} fetchPolicy="network-only">
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error : {error}</p>;
+          return (
+            <DataView
+              value={data.candidates}
+              layout="list"
+              itemTemplate={itemTemplate}
+              rows={20}
+            />
+          );
+        }}
+      </Query>
+    </div>
   );
 }
 
