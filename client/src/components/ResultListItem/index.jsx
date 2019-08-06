2a86e22580360e0f48dd97e2a44fb1fe211d8363
codemaster6152 <punctualityman@outlook.com>
2019-08-06 01:36:54 +0100

    update components

--- client/src/components/ResultListItem/index.jsx
+++ client/src/components/ResultListItem/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Card } from "primereact/card";
 import Avatar from "../Avatar";
 import TextItem from "../TextItem";
@@ -7,9 +8,9 @@ function ResultListItem({ data }) {
   return (
     <Card>
       <div className="p-grid p-align-center">
-        <div className="p-col-4">
+        <Link className="p-col-4" to={`/voter-reason/candidate/${data.id}`}>
           <Avatar url={data.photo} alt="avatar" label={data.vote_type} />
-        </div>
+        </Link>
         <div className="p-col-8">
           <h4>{data.name}</h4>
           <div className="p-grid">
